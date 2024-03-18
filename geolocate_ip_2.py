2fda8fd817af6059900917b65c28a852c4019027
Knup <wxu@DESKTOP-PUU18OI>
2024-03-19 00:21:41 +0100

    feat: new function to retrieve org/timezone

---
+++ geolocate_ip_2.py
@@ -0,0 +1,37 @@
+import asyncio
+import aiohttp
+
+async def geolocate_ip(ip_address):
+    geolocation_url = f'https://ipinfo.io/{ip_address}/json'
+    timeout = 5  # timeout
+
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(geolocation_url, timeout=timeout) as response:
+                if response.status == 200:
+                    data = await response.json()
+                    return {
+                        'ip': ip_address,
+                        'hostname': data.get('hostname', 'N/A'),
+                        'city': data.get('city', 'N/A'),
+                        'region': data.get('region', 'N/A'),
+                        'country': data.get('country', 'N/A'),
+                        'loc': data.get('loc', 'N/A'),
+                        'org': data.get('org', 'N/A'),
+                        'postal': data.get('postal', 'N/A'),
+                        'timezone': data.get('timezone', 'N/A'),
+                    }
+                else:
+                    return {'error': f'Response status {response.status}'}
+        except asyncio.TimeoutError:
+            return {'error': 'Request timed out'}
+        except Exception as e:
+            return {'error': str(e)}
+
+# Usage example with 8.8.8.8
+ip_address = '8.8.8.8'
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    geolocation_info = loop.run_until_complete(geolocate_ip(ip_address))
+    print(geolocation_info)
